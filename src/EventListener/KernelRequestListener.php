d7066d7a278829baee3f5722a7abfed23d97cc71
Lenny4 <alexandre.beaujour@hotmail.fr>
2018-09-05 20:45:32 +0200

    create temp user ID

--- src/EventListener/KernelRequestListener.php
+++ src/EventListener/KernelRequestListener.php
@@ -2,20 +2,26 @@
 
 namespace App\EventListener;
 
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 
 class KernelRequestListener
 {
     private $sam;
+    private $timeCookieTempUser;
 
     /**
      * KernelRequestListener constructor.
      * @param TokenStorage $sam
+     * @param int $timeCookieTempUser
      */
-    public function __construct(TokenStorage $sam)
+    public function __construct(TokenStorage $sam, int $timeCookieTempUser)
     {
         $this->sam = $sam;
+        $this->timeCookieTempUser = $timeCookieTempUser;
     }
 
     /**
@@ -23,10 +29,37 @@ class KernelRequestListener
      */
     public function onKernelRequest(GetResponseEvent $event)
     {
+        $user = $this->sam->getToken()->getUser();
         $route = $event->getRequest()->get('_route');
         if (!($event->isMasterRequest() AND '_wdt' !== $route)) {
             return;
         }
-//        $user = $this->sam->getToken()->getUser();
+    }
+
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        $user = $this->sam->getToken()->getUser();
+        $route = $event->getRequest()->get('_route');
+        if (!($event->isMasterRequest() AND '_wdt' !== $route)) {
+            return;
+        }
+        if (!$user instanceof User) {
+            $this->createTempUser($event);
+        }
+    }
+
+    private function createTempUser(FilterResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        $cookie = $request->cookies;
+        if (!$cookie->has('tempUserId')) {
+            $response = $event->getResponse();
+            $tempUserId = uniqid();
+            $event->getRequest()->attributes->set('tempUserId', $tempUserId);
+            $request = $event->getRequest();
+            $tempUserId = $request->attributes->get('tempUserId');
+            $cookie = new Cookie("tempUserId", $tempUserId, time() + $this->timeCookieTempUser);
+            $response->headers->setCookie($cookie);
+        }
     }
 }
